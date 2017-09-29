200886e1220ec7ac194890b11d7da48f68f15e8b
rahadian347 <mrahadian.permana@gmail.com>
2017-09-29 09:44:59 +0700

    MVC CRUD with MS SQL Server

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php 
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'sqlsrv',
+    'host' => 'localhost',
+    'database' => 'CrudExample',
+    'username' => 'SA',
+    'password' => 'Watchman347',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode-ci',
+    'prefix' => '',
+    'pooling' => false,
+]);
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
